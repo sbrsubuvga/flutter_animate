489ba582241a0b080bd0dfec54f89060ffb5ecd9
subair <subair@lithos.in>
2025-06-19 17:16:59 +0530

    >>>>

--- example/lib/examples/playground_view.dart
+++ example/lib/examples/playground_view.dart
@@ -8,13 +8,36 @@ class PlaygroundView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: DefaultTextStyle(
-        style: const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-        child: const Text("Playground 🛝")
-            .animate()
-            .slideY(duration: 900.ms, curve: Curves.easeOutCubic)
-            .fadeIn(),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Column(
+            children:List.generate(100, (index) {
+              return const Card(
+                child: DefaultTextStyle(
+                  style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+                  child: Text("Playground 🛝")
+
+                ),
+              ).animate().fadeIn(duration: 800.ms)
+                  .slideY(duration: 900.ms, begin: 0.4);
+            }).toList()
+
+          ),
+          Column(
+              children:List.generate(100, (index) {
+                return const Card(
+                  child: DefaultTextStyle(
+                      style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+                      child: Text("Playground 🛝")
+
+                  ),
+                ).animate().fadeIn(duration: 800.ms)
+                    .slideY(duration: 900.ms, begin: 0.4);
+              }).toList()
+
+          )
+        ],
       ),
     );
   }
